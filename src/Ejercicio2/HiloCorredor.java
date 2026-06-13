ae71cd72ad0b0921a53da7058ec6b2f8ffd2fa33
Sharlot <sharlot_adrian@ucne.edu.do>
2026-06-13 16:27:27 -0400

    Creacion de la clase HiloCorredor y su override del metodo run.

---
+++ src/Ejercicio2/HiloCorredor.java
@@ -0,0 +1,25 @@
+package Ejercicio2;
+
+public class HiloCorredor extends Thread {
+
+    public HiloCorredor(String nombre){
+        super(nombre);
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            System.out.println(getName() + " avanzo al metro " + i + ".");
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.getStackTrace();
+            }
+
+        }
+  
+    }
+    
+}
