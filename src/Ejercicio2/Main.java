d81777ec21f820ff6ac9fa2679ce2bd997d542ec
Sharlot <sharlot_adrian@ucne.edu.do>
2026-06-13 17:09:24 -0400

    Creando los objetos juan, pedro, ana y carlos en main

--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -2,6 +2,11 @@ package Ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
+        HiloCorredor juan = new HiloCorredor("Juan");
+        HiloCorredor pedro = new HiloCorredor("Pedro");
+        HiloCorredor ana = new HiloCorredor("Ana");
+        HiloCorredor carlos = new HiloCorredor("Carlos");
         
+
     }
 }
