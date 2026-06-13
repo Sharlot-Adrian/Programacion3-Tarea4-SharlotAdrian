0d5cbd4e820b321ef039cb9012fa39343f2361b3
Sharlot <sharlot_adrian@ucne.edu.do>
2026-06-13 18:02:13 -0400

    Se arreglo un poco el formato de salida al indicar el estado inicial.

--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -8,10 +8,10 @@ public class Main {
         HiloCorredor carlos = new HiloCorredor("Carlos");
         
         System.out.println("Antes de la carrera: ");
-        System.out.println(juan.getState());
-        System.out.println(pedro.getState());
-        System.out.println(ana.getState());
-        System.out.println(carlos.getState());
+        System.out.println(juan.getName() + ": " + juan.getState());
+        System.out.println(pedro.getName() + ": " + pedro.getState());
+        System.out.println(ana.getName() + ": " + ana.getState());
+        System.out.println(carlos.getName() + ": " + carlos.getState());
 
         juan.start();
         pedro.start();
