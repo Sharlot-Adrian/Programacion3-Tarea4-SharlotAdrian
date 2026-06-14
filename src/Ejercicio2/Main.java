7423509b7f2b4cb7106a0c5e73a5417a5a4b6730
Sharlot <sharlot_adrian@ucne.edu.do>
2026-06-14 07:28:23 -0400

    Se implemento el try-catch para poder visualizar el estado de los hilos

--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -2,20 +2,35 @@ package Ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
+
         HiloCorredor juan = new HiloCorredor("Juan");
         HiloCorredor pedro = new HiloCorredor("Pedro");
         HiloCorredor ana = new HiloCorredor("Ana");
         HiloCorredor carlos = new HiloCorredor("Carlos");
+
+        try {
+            
+
+            System.out.println("Antes de la carrera: ");
+            System.out.println(juan.getName() + ": " + juan.getState());
+            System.out.println(pedro.getName() + ": " + pedro.getState());
+            System.out.println(ana.getName() + ": " + ana.getState());
+            System.out.println(carlos.getName() + ": " + carlos.getState());
+
+            juan.start();
+            pedro.start();
+            ana.start();
+            carlos.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
-        System.out.println("Antes de la carrera: ");
+        System.out.println("Despues de la carrera: ");
         System.out.println(juan.getName() + ": " + juan.getState());
         System.out.println(pedro.getName() + ": " + pedro.getState());
         System.out.println(ana.getName() + ": " + ana.getState());
         System.out.println(carlos.getName() + ": " + carlos.getState());
 
-        juan.start();
-        pedro.start();
-        ana.start();
-        carlos.start();
     }
 }
